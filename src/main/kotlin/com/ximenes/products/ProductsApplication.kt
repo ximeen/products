8128db9be81c643d1010bdd94d7ad9319da07e89
ximeen <gabriximens100@gmail.com>
2026-03-31 18:54:41 -0300

    config: define timestamps em UTC
    - Configura TimeZone.setDefault(UTC) na inicialização da aplicação
    - Adiciona configuração de encoding UTF-8
    - Adiciona padrão de log com timezone UTC

--- src/main/kotlin/com/ximenes/products/ProductsApplication.kt
+++ src/main/kotlin/com/ximenes/products/ProductsApplication.kt
@@ -2,10 +2,12 @@ package com.ximenes.products
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
 class ProductsApplication
 
 fun main(args: Array<String>) {
-	runApplication<ProductsApplication>(*args)
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+    runApplication<ProductsApplication>(*args)
 }
